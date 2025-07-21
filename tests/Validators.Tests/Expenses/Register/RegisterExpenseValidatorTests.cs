2761bd304f39e240d57694273bd4fb3b912e3df1
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-07-21 13:29:48 +0200

    test: all tests for errors and improvements

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,8 @@
 ﻿using CashFlow_Application.UseCases.Expenses.Register;
+using CashFlow_Communication.Enums;
 using CashFlow_Exception;
 using CommonTestUtilities.Requests;
+using EmptyFiles;
 using Shouldly;
 
 namespace Validators.Tests.Expenses.Register;
@@ -20,13 +22,16 @@ public class RegisterExpenseValidatorTests
         result.IsValid.ShouldBeTrue();
     }
 
-    [Fact]
-    public void Error_Title_Empty()
+    [Theory]
+    [InlineData("")]
+    [InlineData("    ")]
+    [InlineData(null)]
+    public void Error_Title_Empty(string title)
     {
         //Arrange
         var validator = new RegisterExpenseValidator();
         var request = RequestRegisterExpenseJsonBuilder.Build();
-        request.Title = string.Empty;
+        request.Title = title;
 
         //Act
         var result = validator.Validate(request);
@@ -35,6 +40,99 @@ public class RegisterExpenseValidatorTests
         result.IsValid.ShouldBeFalse();
         result.Errors.Count.ShouldBe(1);
         result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.TITLE_REQUIRED);
+
+    }
+
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(1);
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.DATE_CANNOT_BE_FUTURE);
+
+    }
+
+    [Fact]
+    public void Error_Payment_Method_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.PaymentMethod = (PaymentMethod)700;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.PAYMENT_INVALID_METHOD);
+
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-3)]
+    [InlineData(-24)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Amount = amount;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+
+    }
+
+    [Fact]
+    public void Error_Decription_Empty()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Description = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.DESCRIPTION_REQUIRED);
+
+    }
+
+    [Fact]
+    public void Error_Category_Invalid()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Category = (ExpensesCategories)700;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.CATEGORY_INVALID);
 
     }
 }
