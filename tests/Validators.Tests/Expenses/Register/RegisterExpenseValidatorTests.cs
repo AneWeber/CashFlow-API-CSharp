74603bc248ebe3cb0f194456a76cce765334909b
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-07-21 10:37:38 +0200

    test: Unit test for Validator

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow_Application.UseCases.Expenses.Register;
+using CashFlow_Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson 
+        { 
+            Title = "Test Expense",
+            Description = "This is a test expense",
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Category = CashFlow_Communication.Enums.ExpensesCategories.Miscellaneous,
+            PaymentMethod = CashFlow_Communication.Enums.PaymentMethod.CreditCard
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid, "Validation should succeed for valid expense data.");
+    }
+}
