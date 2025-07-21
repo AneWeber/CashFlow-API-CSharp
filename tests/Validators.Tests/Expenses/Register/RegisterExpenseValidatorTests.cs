da9d1c33eb1b8f6dc4cf69a45ad0bd4b52392d66
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-07-21 13:05:29 +0200

    test: error test - title is empty

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow_Application.UseCases.Expenses.Register;
+using CashFlow_Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -16,6 +17,24 @@ public class RegisterExpenseValidatorTests
         var result = validator.Validate(request);
 
         //Assert - with Shouldly
-        result.ShouldBeNull();
+        result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert - with Shouldly
+        result.IsValid.ShouldBeFalse();
+        result.Errors.Count.ShouldBe(1);
+        result.Errors.ShouldContain(e => e.ErrorMessage == ResourceErrorMessages.TITLE_REQUIRED);
+
     }
 }
