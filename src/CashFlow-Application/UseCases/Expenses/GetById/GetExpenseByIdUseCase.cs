080da5a1495939b4ef85b2d7c548ab83efc2c292
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-08-24 21:54:40 +0200

    feat: logged user to all usecases expenses

--- src/CashFlow-Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow-Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow_Communication.Responses;
 using CashFlow_Domain.Repositories.Expenses;
+using CashFlow_Domain.Services.LoggedUser;
 using CashFlow_Exception;
 using CashFlow_Exception.ExceptionsBase;
 
@@ -9,18 +10,22 @@ public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
 {
     private readonly IExpensesReadOnlyRepository _repository;
     private readonly IMapper _mapper;
+    private readonly ILoggedUser _loggedUser;
 
-    public GetExpenseByIdUseCase(IExpensesReadOnlyRepository repository, IMapper mapper)
+    public GetExpenseByIdUseCase(IExpensesReadOnlyRepository repository, IMapper mapper, ILoggedUser loggedUser)
     {
         _repository = repository;
         _mapper = mapper;
+        _loggedUser = loggedUser;
     }
 
     public async Task<ResponseExpenseJson> Execute(long id)
     {
-        var result = await _repository.GetById(id);
+        var loggedUser = await _loggedUser.Get();
 
-        if(result is null)
+        var result = await _repository.GetById(loggedUser, id);
+
+        if (result is null)
         {
             throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
         }
--- src/CashFlow-Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
+++ src/CashFlow-Domain/Repositories/Expenses/IExpensesWriteOnlyRepository.cs
@@ -5,10 +5,5 @@ public interface IExpensesWriteOnlyRepository
 {
     Task Add(Expense expense);
 
-    /// <summary>
-    /// This function return TRUE if the expense was deleted successfully, otherwise it returns FALSE.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    Task<bool> Delete(long id);
+    Task Delete(long id);
 }
